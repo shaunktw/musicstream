c2aa05b18ff8eccd4d65c028d61694d1d3ba9c83
Shaun <shaunktw@gmail.com>
2014-02-03 23:29:53 -0500

    modified init file

--- app/views.py
+++ app/views.py
@@ -1,11 +1,8 @@
 import os
-
 from flask import render_template, request, send_from_directory
-
 from werkzeug.utils import secure_filename
-
-from app import app 
+from app import app
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
---
+++ run.py
@@ -0,0 +1,6 @@
+import sys
+from app import app
+
+sys.path.insert(0,'path/to/directory')
+
+app.run(debug=True)
