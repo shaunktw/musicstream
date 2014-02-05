52996470b4167db0c984fbb813d1541a99b358a2
Shaun <shaunktw@gmail.com>
2014-02-04 21:37:40 -0500

    modified index file to upload file and stream the audio

--- app/views.py
+++ app/views.py
@@ -6,3 +6,19 @@ from app import app
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/upload', methods=['GET','POST'])
+def upload_file():
+	if request.method == 'POST':
+		file = request.files['file']
+		if file.filename:
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			return render_template('index.html', filename=filename)
+	return render_template('index.html')
+
+@app.route('/uploads/')
+def uploaded_file(filename):
+	return send_from_directory(app.config['UPLOAD_FOLDER']),
+	                           filename, as_attachment=True)
+
